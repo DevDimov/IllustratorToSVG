a8056debe699ad32cf5cac181467978242f65826
Vlad D <77745022+DevDimov@users.noreply.github.com>
2021-02-14 18:37:00 +0000

    Update SaveAsSVG.jsx

--- SaveAsSVG.jsx
+++ SaveAsSVG.jsx
@@ -5,33 +5,33 @@ sourceFolder = Folder.selectDialog('Select the folder with Illustrator files you
 
 // If a valid folder is selected
 if ( sourceFolder != null ) {
-	files = new Array();
-	fileType = prompt('Select type of Illustrator files to you want to process', '*.ai');
+    files = new Array();
+    fileType = prompt('Select type of Illustrator files to you want to process', '*.ai');
 	
-	// Get all files matching the pattern
-	files = sourceFolder.getFiles(fileType);
+    // Get all files matching the pattern
+    files = sourceFolder.getFiles(fileType);
 	
-	if (files.length > 0) {
+    if (files.length > 0) {
         var type = ExportType.SVG;
         for ( i = 0; i < files.length; i++ ) {
-			sourceDoc = app.open(files[i]); // returns the document object
-									
+	    sourceDoc = app.open(files[i]); // returns the document object
+							
             // Save file in the source folder 
             targetFile = new File(sourceFolder);
 			
-			// Call function getSVGOptions to get the ExportOptionsSVG for the files
-			svgSaveOpts = getSVGOptions();
+	    // Call function getSVGOptions to get the ExportOptionsSVG for the files
+	    svgSaveOpts = getSVGOptions();
 			
-			// Save as svg
+	    // Save as svg
             sourceDoc.exportFile(targetFile, type, svgSaveOpts);
             sourceDoc.close(SaveOptions.DONOTSAVECHANGES);
             sourceDoc = null; 
-		}
-		alert('All files saved as SVG in ' + sourceFolder);
 	}
-	else {
-		alert('No matching files found');
-	}
+	alert('All files saved as SVG in ' + sourceFolder);
+    }
+    else {
+        alert('No matching files found');
+    }
 }
 
 function getSVGOptions() {
